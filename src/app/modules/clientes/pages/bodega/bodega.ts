0c7c3ace808d731ae6a28a615056474d74d08a9d
rodrigoioc <isaacolmedochica@gmail.com>
2025-12-16 20:13:26 -0500

    sexo

---
+++ src/app/modules/clientes/pages/bodega/bodega.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bodega',
+  imports: [],
+  templateUrl: './bodega.html',
+  styleUrl: './bodega.css',
+})
+export class Bodega {
+
+}
